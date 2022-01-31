ff8188fdc805dd625d991759fd82c0d1ef326a52
b_gyuwan <gyuwanc@gmail.com>
2022-01-31 22:26:32 +0900

    [add]main page img

--- clone_react01/src/App.js
+++ clone_react01/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './component/Header';
+import Main from './component/Main';
 
 function App() {
   return (
     <div className="App">
-      <Header />       
+      <Header />
+      <Main />       
 
     </div>
   );
---
+++ clone_react01/src/component/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style/mainbanner.scss'
+
+export default function Main() {
+  return(
+    <section id="Main_banner">
+      <div className='explain_box'>
+        <h2><span>오직,</span>너만의 길을 찾아</h2>
+        <p>진로,취준,커리어 고민 OZIC에서 해결해요</p>
+        <p>오디오 커리어콘텐츠 플랫폼 OZIC</p>
+      </div>
+    </section>
+  )
+}
