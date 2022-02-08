87f83561b02e122104249dafc5e8d8368b7afee3
b_gyuwan <gyuwanc@gmail.com>
2022-02-09 00:22:38 +0900

    [add] today.js

--- clone_react01/src/App.js
+++ clone_react01/src/App.js
@@ -4,6 +4,7 @@ import Category from './component/Category';
 import Guide from './component/Guide';
 import Header from './component/Header';
 import Main from './component/Main';
+import Today from './component/Today';
 
 export default App;
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Main />
       <Guide />       
       <Category />
+      <Today />
     </div>
   );
 }
---
+++ clone_react01/src/component/Today.js
@@ -0,0 +1,20 @@
+//today.js
+
+import React from 'react';
+import '../style/today.scss';
+
+export default function Today() {
+  return( 
+  <div id='today_ozic'>
+    <div className='tag_area'>
+      <h2>오늘의 오직</h2>
+      <p className='f_p'>무료</p>
+      <p className='s_p'>새로운 콘텐츠를 만나보세요</p>
+    </div>
+    <ul className='con_area'>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+  )
+}
