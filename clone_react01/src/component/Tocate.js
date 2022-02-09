f74cf950e6f8bcd9f3cecec47636aa57dbc798d8
b_gyuwan <gyuwanc@gmail.com>
2022-02-09 13:39:17 +0900

    [add] router tocate

---
+++ clone_react01/src/component/Tocate.js
@@ -0,0 +1,21 @@
+//tocate.js
+
+
+import React from 'react';
+
+export default function Tocate() {
+  return (
+<ul>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
+  );
+}
--- clone_react01/src/component/Today.js
+++ clone_react01/src/component/Today.js
@@ -1,7 +1,9 @@
 //today.js
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/today.scss';
+import Tocate from './Tocate';
 
 export default function Today() {
   return( 
@@ -12,8 +14,14 @@ export default function Today() {
       <p className='s_p'>새로운 콘텐츠를 만나보세요</p>
     </div>
     <ul className='con_area'>
-      <li></li>
-      <li></li>
+      <li className='bcg_img'>
+        <p className='toozic_01'>식품 생산관리 선배가 답해주는 1문 1답</p>
+        <p className='toozic_02'>궁금한 점은 후기에 작성 부탁 드려요</p>
+        <p className='oziclistens'><Link to="/">듣기</Link></p>
+      </li>
+      <li className='bcg_img_02'>
+        <Tocate />
+      </li>
     </ul>
   </div>
   )
