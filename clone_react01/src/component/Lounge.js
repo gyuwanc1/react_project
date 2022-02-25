af5945240ee39af4d56296d27d5e5c22bf58562d
b_gyuwan <gyuwanc@gmail.com>
2022-02-25 16:14:23 +0900

    [modified] Lounge page source code

--- clone_react01/src/component/Lounge.js
+++ clone_react01/src/component/Lounge.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../style/lounge.scss'
+import Lul from './Lul'
 
 export default function Lounge() {
   return (
     <section id='lounge_area'>
-
+      <h2>lounge</h2>
+      <p>오직 당신만을 위한 혜택과 정보가 한가득</p>
+      <Lul />
     </section>
     
   )
---
+++ clone_react01/src/component/Lul.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../style/lul.scss'
+
+export default function Lul() {
+  return (
+    <ul className='loungebox'>
+      <li>0</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+    </ul>
+  )
+}
